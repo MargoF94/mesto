68b6e012c578e9a6e3b1a4a4eee6932472c6cc80
Margo Fedorovich <localmoyashi@yandex.ru>
2021-10-24 22:50:53 +0900

    Окончательная версия3

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ let popupElement = document.querySelector('.popup');
 let profileFormElement = popupElement.querySelector('.popup__form');
 
 let popupOpenBtn = document.querySelector('.profile__button-edit');
-let popupCloseBtn = popupElement.querySelector('.form__button-close');
+let popupCloseBtn = popupElement.querySelector('.popup__button-close');
 //let formSubmitBtn = popupElement.querySelector('.form__button-submit');
 let profileName = document.querySelector('.profile__title');
 let profileDescription = document.querySelector('.profile__subtitle');
